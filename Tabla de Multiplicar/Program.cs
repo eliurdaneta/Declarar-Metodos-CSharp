a70de1af801f8ad3063bf5a515a3d12d11c691dd
eliurdaneta <eliurdaneta@gmail.com>
2020-03-22 15:41:22 -0300

    Ejemplos de Ejercicio uso de Declaración de Métodos

---
+++ Tabla de Multiplicar/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabla_de_Multipolicar
+{
+   class TablaMultiplicar
+    {
+        string linea;
+        int numero;
+
+        public void CargarDatos()
+        {
+            do
+            {
+                Console.Write("Ingrese Entero (Con -1 se sale)");
+                linea = Console.ReadLine();
+                numero = int.Parse(linea);
+                if(numero != -1)
+                {
+                  CalculaTabla(numero);
+                }
+                
+            }
+            while (numero != 1);
+        }
+
+        public void CalculaTabla(int num)
+        {
+
+            for (int f = num; f <= num * 10; f = f + num)
+            {
+                Console.Write(f + "-");
+            }
+            Console.WriteLine();
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            TablaMultiplicar tm = new TablaMultiplicar();
+            tm.CargarDatos();
+        }
+    }
+}
